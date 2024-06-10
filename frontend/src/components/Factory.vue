474bc51e560f4f470272e88e775d3ee8868f0b76
Ana Edelinski <149072807+ana-edelinski@users.noreply.github.com>
2024-06-10 18:51:14 +0200

    Cards Shadow

--- frontend/src/components/Factory.vue
+++ frontend/src/components/Factory.vue
@@ -98,7 +98,7 @@ header h2 {
 .factory-card {
   background: #fff;
   border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
   overflow: hidden;
   width: 300px;
   text-align: center;
