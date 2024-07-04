b112981fdef0ccd9f759ce9b2a8dbf3b37c663ca
Ana Edelinski <149072807+ana-edelinski@users.noreply.github.com>
2024-07-04 17:01:49 +0200

    feat: Search, filter, sort registered users

--- frontend/src/components/RegisteredUsers.vue
+++ frontend/src/components/RegisteredUsers.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="user-list-container">
-    <h1>Registered Users</h1>
-    <div class="search-container">
-      <div class="search-bar">
-        <div class="search-inputs">
+    <div class="sidebar">
+      <!-- Search bar -->
+      <div class="search-container">
+        <h4>SEARCH</h4>
+        <div class="search-bar">
           <input v-model="searchCriteria.name" type="text" placeholder="Search by name" />
           <input v-model="searchCriteria.surname" type="text" placeholder="Search by surname" />
           <input v-model="searchCriteria.username" type="text" placeholder="Search by username" />
@@ -13,32 +14,75 @@
           <button class="btn btn-clear" @click="clearSearch">Clear</button>
         </div>
       </div>
+
+      <div class="separator"></div>
+
+      <!-- Filter by role -->
+      <div class="filter-container">
+        <h4>FILTER BY ROLE:</h4>
+        <label>
+          <input type="radio" v-model="selectedRole" value="" @change="filterByRole">
+          All Roles
+        </label>
+        <label v-for="role in roles" :key="role">
+          <input type="radio" v-model="selectedRole" :value="role" @change="filterByRole">
+          {{ role }}
+        </label>
+      </div>
     </div>
-    <div class="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th @click="sortBy('username')">Username</th>
-            <th @click="sortBy('name')">First Name</th>
-            <th @click="sortBy('surname')">Last Name</th>
-            <th>Gender</th>
-            <th>Date of Birth</th>
-            <th @click="sortBy('role')">Role</th>
-            <th @click="sortBy('points')">Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="user in sortedUsers" :key="user.username">
-            <td>{{ user.username }}</td>
-            <td>{{ user.name }}</td>
-            <td>{{ user.surname }}</td>
-            <td>{{ user.gender }}</td>
-            <td>{{ formatDate(user.dateOfBirth) }}</td>
-            <td>{{ user.role }}</td>
-            <td>{{ user.points }}</td>
-          </tr>
-        </tbody>
-      </table>
+
+    <!-- User list -->
+    <div class="main-content">
+      <h1>Registered Users</h1>
+      <div class="table-container">
+        <table>
+          <!-- Table headers -->
+          <thead>
+            <tr>
+              <th @click="sortBy('username')" :class="{ 'sorted-asc': sortCriteria.usernameAsc, 'sorted-desc': sortCriteria.usernameDesc }">
+                Username
+                <span v-if="sortCriteria.usernameAsc"><i class="fas fa-sort-up"></i></span>
+                <span v-if="sortCriteria.usernameDesc"><i class="fas fa-sort-down"></i></span>
+              </th>
+              <th @click="sortBy('name')" :class="{ 'sorted-asc': sortCriteria.nameAsc, 'sorted-desc': sortCriteria.nameDesc }">
+                First Name
+                <span v-if="sortCriteria.nameAsc"><i class="fas fa-sort-up"></i></span>
+                <span v-if="sortCriteria.nameDesc"><i class="fas fa-sort-down"></i></span>
+              </th>
+              <th @click="sortBy('surname')" :class="{ 'sorted-asc': sortCriteria.surnameAsc, 'sorted-desc': sortCriteria.surnameDesc }">
+                Last Name
+                <span v-if="sortCriteria.surnameAsc"><i class="fas fa-sort-up"></i></span>
+                <span v-if="sortCriteria.surnameDesc"><i class="fas fa-sort-down"></i></span>
+              </th>
+              <th>Gender</th>
+              <th>Date of Birth</th>
+              <th @click="sortBy('role')" :class="{ 'sorted-asc': sortCriteria.roleAsc, 'sorted-desc': sortCriteria.roleDesc }">
+                Role
+                <span v-if="sortCriteria.roleAsc"><i class="fas fa-sort-up"></i></span>
+                <span v-if="sortCriteria.roleDesc"><i class="fas fa-sort-down"></i></span>
+              </th>
+              <th @click="sortBy('points')" :class="{ 'sorted-asc': sortCriteria.pointsAsc, 'sorted-desc': sortCriteria.pointsDesc }">
+                Points
+                <span v-if="sortCriteria.pointsAsc"><i class="fas fa-sort-up"></i></span>
+                <span v-if="sortCriteria.pointsDesc"><i class="fas fa-sort-down"></i></span>
+              </th>
+            </tr>
+          </thead>
+
+          <!-- Table body -->
+          <tbody>
+            <tr v-for="user in sortedUsers" :key="user.username">
+              <td>{{ user.username }}</td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.surname }}</td>
+              <td>{{ user.gender }}</td>
+              <td>{{ formatDate(user.dateOfBirth) }}</td>
+              <td>{{ user.role }}</td>
+              <td>{{ user.points }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -55,7 +99,7 @@ const searchCriteria = ref({
   username: ''
 });
 const sortCriteria = ref({
-  nameAsc: false,
+  nameAsc: true,
   nameDesc: false,
   surnameAsc: false,
   surnameDesc: false,
@@ -66,6 +110,9 @@ const sortCriteria = ref({
   pointsAsc: false,
   pointsDesc: false
 });
+const roles = ['CUSTOMER', 'MANAGER', 'EMPLOYEE']; 
+
+const selectedRole = ref(''); 
 
 onMounted(() => {
   fetchUsers();
@@ -83,12 +130,18 @@ function fetchUsers() {
 }
 
 function searchUsers() {
-  filteredUsers.value = users.value.filter(user => {
-    const matchesName = user.name.toLowerCase().includes(searchCriteria.value.name.toLowerCase());
-    const matchesSurname = user.surname.toLowerCase().includes(searchCriteria.value.surname.toLowerCase());
-    const matchesUsername = user.username.toLowerCase().includes(searchCriteria.value.username.toLowerCase());
-
-    return matchesName && matchesSurname && matchesUsername;
+  axios.get('http://localhost:8080/chocolate-factory/rest/users/search', {
+    params: {
+      name: searchCriteria.value.name,
+      surname: searchCriteria.value.surname,
+      username: searchCriteria.value.username
+    }
+  })
+  .then(response => {
+    filteredUsers.value = response.data;
+  })
+  .catch(error => {
+    console.error('Error searching users:', error);
   });
 }
 
@@ -98,7 +151,7 @@ function clearSearch() {
     surname: '',
     username: ''
   };
-  filteredUsers.value = users.value; // Reset to show all users
+  filteredUsers.value = users.value; 
 }
 
 const sortedUsers = computed(() => {
@@ -139,88 +192,136 @@ const sortedUsers = computed(() => {
 });
 
 function sortBy(key) {
-  // Reset all sort criteria first
   Object.keys(sortCriteria.value).forEach(criteria => {
-    sortCriteria.value[criteria] = false;
+    if (criteria !== `${key}Asc` && criteria !== `${key}Desc`) {
+      sortCriteria.value[criteria] = false;
+    }
   });
 
-  // Determine the sorting order for the selected key
   if (sortCriteria.value[`${key}Asc`]) {
     sortCriteria.value[`${key}Desc`] = true;
+    sortCriteria.value[`${key}Asc`] = false; 
   } else {
     sortCriteria.value[`${key}Asc`] = true;
+    sortCriteria.value[`${key}Desc`] = false; 
   }
+
+  const order = sortCriteria.value[`${key}Asc`] ? 'asc' : 'desc';
+  axios.get(`http://localhost:8080/chocolate-factory/rest/users/sort`, {
+    params: {
+      sortBy: key,
+      order: order
+    }
+  })
+  .then(response => {
+    filteredUsers.value = response.data;
+  })
+  .catch(error => {
+    console.error('Error sorting users:', error);
+  });
 }
 
 function formatDate(date) {
   const d = new Date(date);
   return d.toLocaleDateString();
 }
+
+function filterByRole() {
+  if (selectedRole.value === '') {
+    filteredUsers.value = users.value; 
+  } else {
+    axios.get(`http://localhost:8080/chocolate-factory/rest/users/filter`, {
+      params: {
+        role: selectedRole.value
+      }
+    })
+    .then(response => {
+      filteredUsers.value = response.data;
+    })
+    .catch(error => {
+      console.error('Error filtering users by role:', error);
+    });
+  }
+}
+
 </script>
 
 <style scoped>
 .user-list-container {
-  max-width: 800px;
-  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
 }
 
-h1 {
-  text-align: center;
-  color: #523F31;
+.sidebar {
+  width: 200px;
+  padding: 20px;
+  border-right: 1px solid #ccc;
+}
+
+.main-content {
+  flex: 1;
+  padding: 20px;
+}
+
+.search-container {
+  margin-bottom: 20px;
+  width: 100%;
+}
+
+.search-bar input {
+  width: 90%;
+  padding: 8px;
+}
+
+.buttons {
+  margin-top: 10px;
+}
+
+.filter-container {
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+.filter-container label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.table-container {
+  overflow-x: auto;
 }
 
 table {
   width: 100%;
   border-collapse: collapse;
-  margin-top: 20px;
 }
 
 th, td {
-  padding: 10px;
-  text-align: center; /* Center content in cells */
-  border-bottom: 1px solid #ddd;
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: left;
 }
 
 th {
-  background-color: #523F31;
+  cursor: pointer;
+  position: relative;
+  background-color: #523F31; 
   color: white;
-  cursor: pointer; /* Add cursor pointer to indicate clickable headers */
 }
 
-tr:hover {
-  background-color: #f1f1f1;
+th.sorted-asc::after {
+  content: '\25b2'; /* unicode for sorting */
+  position: absolute;
+  top: 50%;
+  right: 8px;
+  transform: translateY(-50%);
 }
 
-tbody tr:last-child td {
-  border-bottom: 2px solid #523F31; /* Thicker border for the last row */
-}
-
-.table-container {
-  margin: 0 auto;
-}
-
-.search-container {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  justify-content: space-between;
-  margin-left: 30px;
-}
-
-.search-bar {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.search-inputs {
-  display: flex;
-  margin: 0 auto;
-  gap: 10px;
-  justify-content: space-between;
-}
-
-.buttons {
-  margin-left: 10px; /* Adjust the margin between inputs and buttons */
+th.sorted-desc::after {
+  content: '\25bc'; /* unicode for sorting */
+  position: absolute;
+  top: 50%;
+  right: 8px;
+  transform: translateY(-50%);
 }
 </style>
